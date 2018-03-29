f6adc83d544e0a6572beb79f70a121481c3de28c
ZuraSiprashviliOne <ZuraSiprashviliOne@gmail.com>
2018-03-29 17:19:19 +0400

    prettyone css modified

--- PrettyOne/assets/js/script.js
+++ PrettyOne/assets/js/script.js
@@ -3,4 +3,15 @@ $(document).ready(() => {
     AOS.init({
         duration: 1000
     });
+
+    let line = document.createElement('div');
+    // line.css({
+    //     height: '50px',
+    //     backgroundColor: 'white'
+    // });
+    line.css('height', '50px');
+    for(let i = 0; i < 10; i++){
+        $('#body').append(line);
+    }
+
 });
